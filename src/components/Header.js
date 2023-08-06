501aa8be4b4c398de09a81dc0315d5309c886fdf
Ansh <anshkapoor502@gmail.com>
2023-08-06 23:01:03 +0530

    completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ const Header = () => {
           <h1 style={{ fontFamily: "'Itim'" }}>ANSH KAPOOR</h1>
           <div className="parent-container">
             <div className="profile-image">
-              <img src="https://i.ibb.co/NT4PwJp/20220904.jpg" alt="Profile" />
+              <img src="https://i.ibb.co/cy3fLXw/DSC1155.jpg" alt="Profile" />
             </div>
           </div>
           <nav>
